b25a97794138a63f241929c8420b8f374f72a1e4
Antti Raita <n1472@student.jamk.fi>
2023-11-22 10:47:45 +0200

    teht 6.24 asti

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,23 +1,61 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, updateVote } from './request'
+import { useNotificationDispatch } from './notificationContext'
 
 const App = () => {
+  const queryClient = useQueryClient()
 
-  const handleVote = (anecdote) => {
+  const dispatch = useNotificationDispatch()
+
+  const updateVoteMutation = useMutation(updateVote, {
+    onSuccess: (updatedAnecdote) => {
+
+       const anecdotes = queryClient.getQueryData('anecdotes')
+
+       queryClient.setQueryData('anecdotes', anecdotes.map(anecdote => 
+         anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
+       )) 
+    }
+  }) 
+  
+  const handleVote = async (anecdote) => {
+     updateVoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+
+    await dispatch({ type: 'showNotification', payload: `You voted: ${anecdote.content} !`})
+
+    setTimeout(() => {
+      dispatch({ type: 'hideNotification' })
+    }, 5000)
+    
     console.log('vote')
+
+  }
+  
+  const result = useQuery(
+    'anecdotes',
+    getAnecdotes,
+    {
+      retry: 1,
+      refetchOnWindowFocus: false
+    }
+  )
+  console.log(result)
+
+  if ( result.isLoading ) {
+        return <div>Loading data...</div>
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if ( result.isError ) {
+    return <div>Anecdote service not avaiable due to problems in server</div>
+}
+
+const anecdotes = result.data
 
   return (
     <div>
-      <h3>Anecdote app</h3>
+      <h1>Anecdote app</h1>
     
       <Notification />
       <AnecdoteForm />
@@ -29,12 +67,13 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
+            <button onClick={() => handleVote(anecdote)}>Vote</button>
           </div>
         </div>
       )}
     </div>
   )
 }
+
 
 export default App
